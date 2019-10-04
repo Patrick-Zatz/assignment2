7ffbd0c65fe2ee47307777953f3bf4d228eb49a3
Patrick-Zatz <patrickz678@yahoo.com>
2019-10-04 11:28:07 -0400

    Added milageEstimate to Jet.cpp
    The mileageEstimate will set the fule type to Rocket if there are more than 2 engines. Then the mileage is a random number between 40-100. If the fule type is Rocket then the mileage is boosted by 5.5% per engine. the function then returns the mileage.

--- Jet.cpp
+++ Jet.cpp
@@ -20,3 +20,14 @@ void Jet::setNumEngines(int numEngines){
     else if(numEngines <= 0)
         numberOfEngines = 0;
 }
+
+
+double Jet::mileageEstimate(double time) {
+    if(numberOfEngines > 2)
+        setFuelType("Rocket");
+    double mileage = (rand()%61)+40;
+    if (fuelType == "Rocket") {
+        mileage += mileage * (0.055 * numberOfEngines);
+    }
+    return mileage;
+}
